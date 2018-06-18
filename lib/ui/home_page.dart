db8e788f106c05057bd8f180fd156cec77311b23
Ryan Miles <ryanm1114@gmail.com>
2018-06-18 16:59:15 -0400

    title and center text

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,17 +1,40 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(
-          backgroundColor: Colors.green,
+          backgroundColor: Colors.lightGreen,
           title: new Text("Make It Rain!"),
         ),
         body: new Container(
           alignment: Alignment.center,
           child: Column(
-            children: <Widget>[new Text("Hi")],
+            children: <Widget>[
+              new Text(
+                "Get Rich!",
+                style: new TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w800,
+                  fontSize: 29.9,
+                ),
+              ),
+              new Expanded(
+                  child: new Center(
+                      child: new Text("Money!",
+                          style: new TextStyle(
+                              color: Colors.greenAccent,
+                              fontSize: 45.0,
+                              fontWeight: FontWeight.w400))))
+            ],
           ),
         ));
   }
